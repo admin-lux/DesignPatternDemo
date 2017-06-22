35badbe13d7722dc8bc6994bce935b17c3eccf5f
MICROSO-8M1HDVQ\MyPC <xxuelc@icloud.com>
2017-06-22 15:30:49 +0800

    Singleton设计模式

--- src/com/rick/designpattern/singleton/Client.java
+++ src/com/rick/designpattern/singleton/Client.java
@@ -7,7 +7,7 @@ public class Client {
 
     public static void main(String[] args) {
         MazeGame mazeGame = new MazeGame();
-        BombedMazeFactory bombedMazeFactory = new BombedMazeFactory();
+        BombedMazeFactory bombedMazeFactory = BombedMazeFactory.instance();
         mazeGame.createMaze(bombedMazeFactory);
     }
 }
